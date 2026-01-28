be0804ffa48548d330f87236f542e798efdfbf66
Axel Gérard <2442326@cegeprdl.ca>
2026-01-28 15:35:58 -0500

    Test réalisé et fonctionnel

--- playwrightTests/Helper/Mocks/category.mock.ts
+++ playwrightTests/Helper/Mocks/category.mock.ts
@@ -23,4 +23,48 @@ export const categoryMocks = {
       },
     },
   },
+  getCategoryForProject1Success: {
+    url: "/project/1/categories",
+    method: "GET",
+    response: {
+      status: 200,
+      json: {
+        categories: [
+          {
+            id: 1,
+            name: "Design",
+            description: "Tâches de design",
+            billable: true,
+            activities: [],
+            createdAt: "2025-04-02T10:29:54-04:00",
+            updatedAt: "2025-04-02T10:29:54-04:00",
+            userId: 1,
+            projectId: 1,
+          },
+        ],
+      },
+    },
+  },
+  getCategoryForProject2Success: {
+    url: "/project/2/categories",
+    method: "GET",
+    response: {
+      status: 200,
+      json: {
+        categories: [
+          {
+            id: 2,
+            name: "Développement",
+            description: "Tâches de développement",
+            billable: true,
+            activities: [],
+            createdAt: "2025-04-02T10:29:54-04:00",
+            updatedAt: "2025-04-02T10:29:54-04:00",
+            userId: 1,
+            projectId: 2,
+          },
+        ],
+      },
+    },
+  },
 } satisfies Record<string, MockConfig>;
