1a25a2eded8bc2efe583b416446f7e9c71067081
yyyany <2332491@cegeprdl.ca>
2026-04-03 10:06:58 -0400

    test: Changement du test par un autre qui fontionne juste pour voir si cest mon test qui pose probleme

--- playwrightTests/tests/test.checkFilterProjectWithDate.spec.ts
+++ playwrightTests/tests/test.checkFilterProjectWithDate.spec.ts
@@ -2,61 +2,61 @@ import { test, expect } from "@playwright/test";
 import { ApiMocker } from "../Helper/mockApi";
 import { projectMocks } from "../Helper/Mocks/project.mock";
 import { userMocks } from "../Helper/Mocks/user.Mock";
+import { activityMocks } from "../Helper/Mocks/activity.mock";
 
-test.beforeEach(async ({ page }) => {
-  const apiMocker = new ApiMocker(page);
-  await apiMocker
-    .addMocks([
-      projectMocks.getDetailedProjectsSuccess,
-      userMocks.userMeSuccess,
-    ])
-    .apply();
-
-  await page.clock.install({ time: new Date("2025-03-22T08:00:00") });
-  await page.goto("http://localhost:5002/projects");
-  await page.waitForLoadState("networkidle");
-});
-
-test.describe("Recherche et filtrage de projets", () => {
-  
-  test("filtre les projets par texte en temps réel", async ({ page }) => {
-    await page.waitForSelector('[data-testid="project-item"]');
-    await expect(page.locator('[data-testid="project-item"]')).toHaveCount(8);
-
-    const searchInput = page.getByTestId("project-search");
-    await searchInput.fill("AT-123");
-
-    const visibleProjects = page.locator('[data-testid="project-item"]');
-    await expect(visibleProjects).toHaveCount(1);
-    await expect(visibleProjects.first()).toContainText("AT-123");
+test.describe("checkRemainingHoursColor", () => {
+  test.beforeEach(async ({ page }) => {
+    const apiMocker = new ApiMocker(page);
+    await apiMocker
+      .addMocks([
+        userMocks.userMeSuccess,
+        activityMocks.getAllActivitiesDefaultWeekSuccess,
+        projectMocks.getProjectsListSuccess,
+        projectMocks.getDetailedProjectsByUserSuccess,
+      ])
+      .apply();
+    await page.clock.install({ time: new Date("2025-03-22T08:00:00") });
+    await page.goto("http://localhost:5002/calendar");
+    await page.waitForLoadState("networkidle");
   });
 
-test("filtre les projets avec les dates de début et de fin via l'API", async ({ page }) => {
-    const testStartDate = "2025-01-01";
-    const testEndDate = "2025-01-31";
+  test("negativeRemainingHours", async ({ page }) => {
+    await page.getByRole("button", { name: "new-1 | new commut" }).click();
+    await page.waitForSelector(".text-red-700", { state: "visible" });
 
-    const mockFilteredProject = projectMocks.getDetailedProjectsSuccess.response.json.projects[0];
+    const redValues = page
+      .locator(".text-red-700")
+      .filter({ hasText: "-4h00" });
+    await expect(redValues).toHaveCount(2);
+    await expect(redValues.first()).toBeVisible();
+  });
+  test("positiveRemainingHours", async ({ page }) => {
+    await page.getByRole("button", { name: "migr-2 | projet ! apre" }).click();
+    await page.waitForSelector(".text-gray-700", { state: "visible" });
+    const greyValues = page
+      .locator(".text-gray-700")
+      .filter({ hasText: "-1h00" });
+    await expect(greyValues).toHaveCount(2);
+    await expect(greyValues.first()).toBeVisible();
+  });
 
-    const dynamicMocker = new ApiMocker(page);
-    await dynamicMocker.addMock({
-      url: `/projects/detailed?from=${testStartDate}&to=${testEndDate}`,
-      method: "GET",
-      response: {
-        status: 200,
-        json: {
-          projects: [mockFilteredProject]
-        }
-      }
-    }).apply();
+  test("noEstimatedHours", async ({ page }) => {
+    await page.getByRole("button", { name: "-mmm | le nom !" }).click();
+    await page.waitForSelector(".text-gray-700", { state: "visible" });
+    const greyValues = page
+      .locator(".text-gray-700")
+      .filter({ hasText: "-2h30" });
+    await expect(greyValues).toHaveCount(2);
+    await expect(greyValues.first()).toBeVisible();
+  });
 
-    await page.waitForSelector('[data-testid="project-item"]');
-
-    await page.locator('#startDate').fill(testStartDate);
-    await page.locator('#endDate').fill(testEndDate);
-    await page.locator('#endDate').blur(); 
-
-    const visibleProjects = page.locator('[data-testid="project-item"]');
-    await expect(visibleProjects).toHaveCount(1, { timeout: 10000 });
-    await expect(visibleProjects.first()).toContainText(mockFilteredProject.name);
+  test("noRemainingHasEstimate", async ({ page }) => {
+    await page
+      .getByRole("button", { name: "10htotal | 10 heures au total" })
+      .click();
+    await page.waitForSelector(".text-gray-700", { state: "visible" });
+    const NoneValues = page.locator(".text-gray-400").filter({ hasText: "-" });
+    await expect(NoneValues).toHaveCount(2);
+    await expect(NoneValues.first()).toBeVisible();
   });
 });
