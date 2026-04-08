3a2beda3bf5d4319aceafa6276ac68e1d7e1424f
Axel Gérard <2442326@cegeprdl.ca>
2026-04-08 11:07:09 -0400

    fix(reportController): suppression de vérifications déjà faites

--- API/controllers/report_controller.go
+++ API/controllers/report_controller.go
@@ -1,45 +1,20 @@
 package controllers
 
 import (
-	"net/http"
-
-	"llio-api/models/DTOs"
-	"llio-api/models/enums"
 	"llio-api/services"
 
 	"github.com/gin-gonic/gin"
 )
 
-var rapportSTR = "rapport"
-
 func ExportExcel(c *gin.Context) {
-	currentUser, exists := c.Get("current_user")
-
-	if !exists {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Utilisateur non authentifié"})
-		return
-	}
-
-	user, ok := currentUser.(*DTOs.UserDTO)
-	if !ok {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erreur interne du serveur"})
-		c.Abort()
-		return
-	}
-
-	if user.Role != enums.Administrator{
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Utilisateur n'est pas Administrateur'"})
-		return
-	}
-
 	// Headers download
 	c.Header("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 	c.Header("Content-Disposition", "attachment; filename=activities.xlsx")
-	c.Header("Transfer-Encoding", "chunked") // 🔥 important pour gros fichiers
+	c.Header("Transfer-Encoding", "chunked")
 
     err := services.GenerateExcel(c.Writer)
     if err != nil {
-        handleError(c, err, rapportSTR)
+        handleError(c, err, "ExportExcel")
         return
     }
 }
