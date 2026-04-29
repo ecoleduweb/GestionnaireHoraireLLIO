4b5d178fdafbb3875943ca1e59a435274e7c9a5e
Edgar Gegout <2435196@cegeprdl.ca>
2026-04-29 14:38:39 -0400

    FEAT : TestbeforeEach pour le test outlook + Mock getEvenementWithoutIdFromOutlook

--- playwrightTests/Helper/Mocks/outlook.mock.ts
+++ playwrightTests/Helper/Mocks/outlook.mock.ts
@@ -1,3 +1,30 @@
 import { MockConfig } from "../types";
 
-export const outlookMocks = {};
+export const outlookMocks = {
+  getActivityWithoutProjectIdFromOutlook: {
+    url: "/activities/me/outlook?date=2026-05-01",
+    method: "GET",
+    response: {
+      status: 200,
+      json: {
+        date: "2026-05-01",
+        events: [
+          {
+            id: "AAMkAGU2NjNjZjY5LWJiNTktNDk3YS1iOTA3LWRjZjJkYzg2YzI4MwBGAAAAAACobdNDVfcWRrhq17eN0GJQBwC9ZRNNh3HrTpUd-xS6ahyvAAAAAAENAAC9ZRNNh3HrTpUd-xS6ahyvAAG-SjUyAAA=",
+            subject: "Test sans Id Projet",
+            body: { content: "\r\n" },
+            start: {
+              dateTime: "2026-05-01T15:00:00.0000000",
+              timeZone: "UTC",
+            },
+            end: {
+              dateTime: "2026-05-01T16:00:00.0000000",
+              timeZone: "UTC",
+            },
+            isAllDay: false,
+          },
+        ],
+      },
+    },
+  },
+};
--- playwrightTests/tests/test.outlookImport.spec.ts
+++ playwrightTests/tests/test.outlookImport.spec.ts
@@ -1,3 +1,22 @@
-import { test, expect } from '@playwright/test';
-import { ApiMocker } from '../Helper/mockApi';
-import { userMocks } from '../Helper/Mocks/user.Mock';
+import { test, expect } from "@playwright/test";
+import { ApiMocker } from "../Helper/mockApi";
+import { userMocks } from "../Helper/Mocks/user.Mock";
+import { outlookMocks } from "../Helper/Mocks/outlook.mock";
+import { projectMocks } from "../Helper/Mocks/project.mock";
+import { categoryMocks } from "../Helper/Mocks/category.mock";
+
+test.describe("ImportOutlook", () => {
+  test.beforeEach(async ({ page }) => {
+    const apiMocker = new ApiMocker(page);
+    await apiMocker
+      .addMocks([
+        projectMocks.getDetailedProjectsByUserSuccess,
+        categoryMocks.getCategoriesByProjectSuccess,
+        userMocks.userMeSuccess,
+      ])
+      .apply();
+    await page.clock.install({ time: new Date("2026-05-01T06:00:00") });
+    await page.goto("http://localhost:5002/projects");
+    await page.waitForLoadState("networkidle");
+  });
+});
