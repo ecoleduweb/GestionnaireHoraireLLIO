5895a2291ba28faf863399d3a69be51537a2fb42
Edgar Gegout <2435196@cegeprdl.ca>
2026-04-29 08:42:11 -0400

    Fix : Ajout du svg dans les assets

--- LLIO2025/src/Components/Projects/ProjectComponent.svelte
+++ LLIO2025/src/Components/Projects/ProjectComponent.svelte
@@ -9,6 +9,7 @@
   import GenerateTableCategories from './GenerateTableCategories.svelte';
   import { CategoryApiService } from '../../services/CategoryApiService';
   import Modal from '../Modal/BaseModal.svelte';
+  import CrayonEdit from '../../assets/svg/crayon.svg.svelte';
 
   let {project, onClickAddCoManager = () => {},onClickReassignManager = () => {}}: {project: any;onClickAddCoManager?: () => void;onClickReassignManager?: () => void;} = $props();
   let isDetailsVisible = $state([]);
@@ -105,11 +106,7 @@
                         type="button"
                         class="p-1 rounded-md hover:bg-gray-200 flex items-center justify-center"
                       >
-                        <svg class="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15.232 5.232l3.536 3.536M9 13l6.232-6.232a2 2 0 112.828 2.828L11.828 16.828a4 4 0 01-1.414.94L7 19l1.232-3.414a4 4 0 01.94-1.414z"
-                          />
-                        </svg>
+                        <CrayonEdit/>
                       </button>
                     </div>
 
---
+++ LLIO2025/src/assets/svg/crayon.svg.svelte
@@ -0,0 +1,5 @@
+<svg class="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+d="M15.232 5.232l3.536 3.536M9 13l6.232-6.232a2 2 0 112.828 2.828L11.828 16.828a4 4 0 01-1.414.94L7 19l1.232-3.414a4 4 0 01.94-1.414z"
+/>
+</svg>
