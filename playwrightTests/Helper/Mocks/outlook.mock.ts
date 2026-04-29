7525351e99f7a3ed175a150037b6508a208fcca4
Edgar Gegout <2435196@cegeprdl.ca>
2026-04-29 15:37:14 -0400

    FEAT : test de l'ajout d'une activité depuis le calendrier outlook

--- playwrightTests/Helper/Mocks/outlook.mock.ts
+++ playwrightTests/Helper/Mocks/outlook.mock.ts
@@ -2,23 +2,23 @@ import { MockConfig } from "../types";
 
 export const outlookMocks = {
   getActivityWithoutProjectIdFromOutlook: {
-    url: "/activities/me/outlook?date=2026-05-01",
+    url: "/activities/me/outlook?date=2025-03-22",
     method: "GET",
     response: {
       status: 200,
       json: {
-        date: "2026-05-01",
+        date: "2025-03-22",
         events: [
           {
             id: "AAMkAGU2NjNjZjY5LWJiNTktNDk3YS1iOTA3LWRjZjJkYzg2YzI4MwBGAAAAAACobdNDVfcWRrhq17eN0GJQBwC9ZRNNh3HrTpUd-xS6ahyvAAAAAAENAAC9ZRNNh3HrTpUd-xS6ahyvAAG-SjUyAAA=",
             subject: "Test sans Id Projet",
             body: { content: "\r\n" },
             start: {
-              dateTime: "2026-05-01T15:00:00.0000000",
+              dateTime: "2025-03-22T15:00:00.0000000",
               timeZone: "UTC",
             },
             end: {
-              dateTime: "2026-05-01T16:00:00.0000000",
+              dateTime: "2025-03-22T16:00:00.0000000",
               timeZone: "UTC",
             },
             isAllDay: false,
--- playwrightTests/tests/test.outlookImport.spec.ts
+++ playwrightTests/tests/test.outlookImport.spec.ts
@@ -4,6 +4,7 @@ import { userMocks } from "../Helper/Mocks/user.Mock";
 import { outlookMocks } from "../Helper/Mocks/outlook.mock";
 import { projectMocks } from "../Helper/Mocks/project.mock";
 import { categoryMocks } from "../Helper/Mocks/category.mock";
+import { activityMocks } from "../Helper/Mocks/activity.mock";
 
 test.describe("ImportOutlook", () => {
   test.beforeEach(async ({ page }) => {
@@ -11,12 +12,55 @@ test.describe("ImportOutlook", () => {
     await apiMocker
       .addMocks([
         projectMocks.getDetailedProjectsByUserSuccess,
-        categoryMocks.getCategoriesByProjectSuccess,
+        categoryMocks.getCategoryForProject1Success,
+        projectMocks.getProjectsListSuccess,
         userMocks.userMeSuccess,
+        activityMocks.getAllActivityEmpty,
+        userMocks.getTimeBankSuccess,
+        userMocks.getTimeBankConfigSuccess,
       ])
       .apply();
-    await page.clock.install({ time: new Date("2026-05-01T06:00:00") });
-    await page.goto("http://localhost:5002/projects");
+    await page.clock.install({ time: new Date("2025-03-22T08:00:00-04:00") });
+    await page.goto("http://localhost:5002/calendar");
     await page.waitForLoadState("networkidle");
+  });
+  test("AddActivityFromOutlookWithoutProjetId", async ({ page }) => {
+    const mocker = new ApiMocker(page);
+    await mocker
+      .addMocks([
+        outlookMocks.getActivityWithoutProjectIdFromOutlook,
+        userMocks.userMeSuccess,
+        activityMocks.addActivitySuccess,
+      ])
+      .apply();
+
+    await page.reload();
+    await page.waitForLoadState("networkidle");
+    await page
+      .locator('[data-date="2025-03-22"] button:has-text("+ Outlook")')
+      .click();
+    await expect(page.getByPlaceholder("Sélectionner un projet")).toBeVisible();
+    await page.getByRole("textbox", { name: "Projet *" }).click();
+    await page
+      .getByText("Projet sous-sol | Nommer le projet", { exact: true })
+      .click();
+
+    await expect(
+      page.getByRole("textbox", { name: "Nom (optionnel)" }),
+    ).toHaveValue("Test sans Id Projet");
+
+    const activityRequest = page.waitForRequest(
+      (request) =>
+        request.url().includes("/activity") && request.method() === "POST",
+    );
+
+    await page.getByRole("button", { name: "Importer", exact: true }).click();
+
+    const request = await activityRequest;
+    expect(request.method()).toBe("POST");
+
+    await expect(
+      page.getByText("Importation des évènements Outlook"),
+    ).not.toBeVisible();
   });
 });
