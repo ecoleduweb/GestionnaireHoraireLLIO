0a1bd86a7d2fb44d47f413a3dd25b6273cf0842c
Axel Gérard <2442326@cegeprdl.ca>
2026-04-29 14:42:21 -0400

    feat(test): ajout axel

--- playwrightTests/Helper/Mocks/outlook.mock.ts
+++ playwrightTests/Helper/Mocks/outlook.mock.ts
@@ -1,3 +1,32 @@
 import { MockConfig } from "../types";
 
-export const outlookMocks = {};
+export const outlookMocks = {
+  getEventsFromOutlookSuccess: {
+    url: "/activities/me/outlook?date=2026-05-01",
+    method: "GET",
+    response: {
+      status: 200,
+      json: {
+        date: "2026-05-01",
+        events: [
+          {
+            id: "AAMkAGQ2MDNhYjVjLWFkMWMtNGNmZi05ZmVjLTdmZmQ4NjgwMjdkNwBGAAAAAABXx3nPK_GjQbVWwm_t6k5tBwAmsLVGJoOjQZrgmFxGm6c6AAAAAAENAAAmsLVGJoOjQZrgmFxGm6c6AADJTqHnAAA=",
+            subject: "AR145",
+            body: { content: "\r\n" },
+            start: { dateTime: "2026-05-01T12:00:00.0000000", timeZone: "UTC" },
+            end: { dateTime: "2026-05-01T12:30:00.0000000", timeZone: "UTC" },
+            isAllDay: false,
+          },
+          {
+            id: "AAMkAGQ2MDNhYjVjLWFkMWMtNGNmZi05ZmVjLTdmZmQ4NjgwMjdkNwBGAAAAAABXx3nPK_GjQbVWwm_t6k5tBwAmsLVGJoOjQZrgmFxGm6c6AAAAAAENAAAmsLVGJoOjQZrgmFxGm6c6AADJTqHoAAA=",
+            subject: "ahahaha",
+            body: { content: "\r\n" },
+            start: { dateTime: "2026-05-01T14:30:00.0000000", timeZone: "UTC" },
+            end: { dateTime: "2026-05-01T17:30:00.0000000", timeZone: "UTC" },
+            isAllDay: false,
+          },
+        ],
+      },
+    },
+  },
+};
--- playwrightTests/tests/test.outlookImport.spec.ts
+++ playwrightTests/tests/test.outlookImport.spec.ts
@@ -1,3 +1,4 @@
 import { test, expect } from '@playwright/test';
 import { ApiMocker } from '../Helper/mockApi';
 import { userMocks } from '../Helper/Mocks/user.Mock';
+
